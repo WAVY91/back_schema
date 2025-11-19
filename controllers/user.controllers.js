e315c893c4064b1363f4a6b3adcbfc431605fbcd
ajakaiyetomisin@gmail.com <ajakaiyetomisin@gmail.com>
2025-11-19 10:30:22 +0100

    den

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -118,8 +118,8 @@ const postSignIn = async (req, res) => {
       });
     }
 
-    const token = jwt.sign({id: user._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'})
-    console.log(process.env.JWT_SECRET)
+    const token = jwt.sign({id: user._id, email: user.email}, JWT_SECRET, {expiresIn: '1d'})
+    console.log(JWT_SECRET)
 
     // If everything is correct
     return res.status(200).json({
